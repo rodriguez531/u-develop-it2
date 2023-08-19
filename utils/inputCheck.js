c7e8aea38cab8e07b1f6a8962dfc6cb0b713a266
rodriguez531 <alexr5408@gmail.com>
2023-08-19 14:58:15 -0400

    votes route completed

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -2,20 +2,20 @@
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
 module.exports = function(obj, ...props) {
-  const errors = inputCheck(req.body, 'party_id');
+  const errors = [];
 
-  
-    props.forEach((prop) => {
-      // if property is blank or doesn't exist, add to errors array
-      if (obj[prop] === undefined || obj[prop] === '') {
-        errors.push(`No ${prop} specified.`);
-      }
-    });
-  
-    if (errors) {
-      res.status(400).json({ error: errors });
-      return;
+  props.forEach(prop => {
+    // if property is blank or doesn't exist, add to errors array
+    if (obj[prop] === undefined || obj[prop] === '') {
+      errors.push(`No ${prop} specified.`);
     }
-    
-    return null;
-  };
+  });
+
+  if (errors.length) {
+    return {
+      error: errors.join(' ')
+    };
+  }
+
+  return null;
+};
